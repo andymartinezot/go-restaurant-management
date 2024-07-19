562217beeca03850699a8191d31bda7cf4212e5a
Andy Martinez <andy.martinez.-nd@disney.com>
2024-07-19 16:43:59 -0300

    Create Database connection

--- server/internal/database/databaseConnection.go
+++ server/internal/database/databaseConnection.go
@@ -1,33 +1,41 @@
-// package database
+package database
 
-// import(
-// 	"context"
-// 	"fmt"
-// 	"log"
-// 	"time"
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"go.mongodb.org/mongo-driver/options"
-// )
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-// func DBinstance() *mongo.Client{
-// 	MongoDb := "mongodb://localhost:27017"
-// 	fmt.Println(MongoDb)
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/options"
+)
 
-// 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
-// 	if err !=nil{
-// 		log.Fatal(err)
-// 	}
-	
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
+	fmt.Println(MongoDb)
 
-// 	defer cancel()
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	err = client.Connect(ctx)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
-// 	if err != nil{
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println("Connected to mongodb")
-// 	return client
-// }
+	defer cancel()
 
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return collection
+}
